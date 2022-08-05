f57ae06259185d08cfed63a9800a63cfbbe87648
John <jsaita48@gmail.com>
2022-08-05 16:01:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+const name1 =["Guadalupe","Ollie","Aki"]
+
+ function writeCards(){
+  return  [`Thank you, ${name1[0]}, for the wonderful surprise gift!`,`Thank you, ${name1[1]}, for the wonderful surprise gift!`,`Thank you, ${name1[2]}, for the wonderful surprise gift!`]
+}
+function countDown(){
+
+  debugger;
+  for(let i=10;i>=0;i--){
+    console.log(i)
+    debugger;
+
+  }
+
+
+}
